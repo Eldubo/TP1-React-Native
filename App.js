a151f2b66232d7c59bbb5e371ee0f8ef5417d3d2
Matias Dubin <magondubi@gmail.com>
2025-04-28 10:37:51 -0300

    funciona bien el ejemplo

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const esperarAlgunosMilisegundos = async () => {
+      try {
+        // Evitar que el splash screen se oculte automáticamente
+        await SplashScreen.preventAutoHideAsync();
+
+        // Simular una demora de 5 segundos
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Cambiar el estado de carga a falso y ocultar el splash screen
+        setIsLoading(false);
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    esperarAlgunosMilisegundos();
+  }, []);
+
+  // Renderizado condicional: mostrar una pantalla en blanco mientras se carga
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Open up App.js to start working in your app!</Text>
     </View>
   );
 }
@@ -13,8 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
